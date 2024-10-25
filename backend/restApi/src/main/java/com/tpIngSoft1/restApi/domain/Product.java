d7930279dc73e2e1123e944102d8666b093c4fd0
tomascastroo <tocastro@fi.uba.ar>
2024-10-25 00:57:56 -0300

    Subo estructura de backend

---
+++ backend/restApi/src/main/java/com/tpIngSoft1/restApi/domain/Product.java
@@ -0,0 +1,16 @@
+package com.tpIngSoft1.restApi.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Document
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String name;
+    private String description;
+}

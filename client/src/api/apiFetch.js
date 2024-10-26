7458aa7d3f48c9ab9bf04a6d1f92095156c9b6a8
facundotfernandez <ftfernandez@fi.uba.ar>
2024-10-26 02:18:22 -0300

    feat(client): cambios menores
    Cambios menores, implementados axios create básico  y simulacion de fetch de productos

--- client/src/api/apiFetch.js
+++ client/src/api/apiFetch.js
@@ -2,15 +2,80 @@ import axios from 'axios';
 import {API_URL, LOCAL_API_URL} from "@utils/common";
 
 export const fetchFromApi = async (key, id) => {
-    const mockData = {
-        orders: [
-            { id: 1, name: 'Order 1' },
-            { id: 2, name: 'Order 2' }
-        ]
+    const mockDataProducts = {
+        products: [{
+            _id: 0x000001,
+            idProduct: "613561565",
+            category: 'Calzados',
+            subCategory: 'Botines',
+            brand: 'Adidas',
+            title: 'F50 Elite',
+            imageUrl: '',
+            price: 100,
+            stock: [{
+                size: '40',
+                color: 'Rojo',
+                amount: 10,
+            }, {
+                size: '41',
+                color: 'Azul',
+                amount: 5,
+            }]
+        }, {
+            _id: 0x000002,
+            category: 'Calzados',
+            subCategory: 'Zapatillas',
+            brand: 'Adidas',
+            title: 'Galaxy Star',
+            imageUrl: '',
+            stock: [{
+                size: '39',
+                color: 'Negro',
+                amount: 15,
+            }, {
+                size: '40',
+                color: 'Blanco',
+                amount: 8,
+            }]
+        }, {
+            _id: 0x000003,
+            category: 'Indumentaria',
+            subCategory: 'Remera',
+            brand: 'Puma',
+            title: 'Remera',
+            imageUrl: '',
+            stock: [{
+                size: 'M',
+                color: 'Rojo',
+                sleeves: 'Corta',
+                amount: 20,
+            }, {
+                size: 'L',
+                color: 'Verde',
+                sleeves: 'Larga',
+                amount: 12,
+            }]
+        }, {
+            _id: 0x000004,
+            category: 'Calzados',
+            subCategory: 'Botines',
+            brand: 'Puma',
+            title: 'Borussia',
+            imageUrl: '',
+            stock: [{
+                size: '42',
+                color: 'Negro',
+                amount: 6,
+            }, {
+                size: '43',
+                color: 'Blanco',
+                amount: 4,
+            }]
+        }]
     };
 
     return new Promise((resolve) => {
-        setTimeout(() => resolve(mockData[key] || []), 1000); // Simular retardo de red
+        setTimeout(() => resolve(mockDataProducts[key] || []), 1000); // Simula retardo de red
     });
 
     // Probar cuando esté la API armada

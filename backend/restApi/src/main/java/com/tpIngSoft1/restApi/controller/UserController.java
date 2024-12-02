44b3a9ee041f960b23dd367e68989b65c75e8c56
tomascastroo <tocastro@fi.uba.ar>
2024-12-02 15:57:35 -0300

    subo cambios en user controller, y authservice

--- backend/restApi/src/main/java/com/tpIngSoft1/restApi/controller/UserController.java
+++ backend/restApi/src/main/java/com/tpIngSoft1/restApi/controller/UserController.java
@@ -1,67 +1,46 @@
 package com.tpIngSoft1.restApi.controller;
 
-import com.tpIngSoft1.restApi.domain.Order;
+import com.tpIngSoft1.restApi.domain.User;
+import com.tpIngSoft1.restApi.dto.UserDTO;
+import com.tpIngSoft1.restApi.service.AuthService;
+import com.tpIngSoft1.restApi.service.JwtService;
+import com.tpIngSoft1.restApi.service.UserService;
+import com.tpIngSoft1.restApi.utils.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
-import com.tpIngSoft1.restApi.domain.User;
-import com.tpIngSoft1.restApi.service.JwtService;
-import org.springframework.validation.annotation.Validated;
-import com.tpIngSoft1.restApi.service.UserService;
-import com.tpIngSoft1.restApi.dto.UserDTO;
-import com.tpIngSoft1.restApi.utils.ApiResponse;
 
-import java.util.*;
-
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping()
-@Validated
 @RequiredArgsConstructor
 @CrossOrigin(origins = {"http://localhost:3000", "https://ing-soft-tp-grupal.vercel.app"})
 public class UserController {
 
     @Autowired
-    private UserService userService;
-
+    private final UserService userService;
     @Autowired
-    private JwtService jwtService;
-
-    private String[] decodeBasicAuth(String authHeader) {
-        if (authHeader != null && authHeader.startsWith("Basic ")) {
-            String base64Credentials = authHeader.substring(6);
-            byte[] decodedBytes = Base64.getDecoder().decode(base64Credentials);
-            String decodedString = new String(decodedBytes);
-            return decodedString.split(":");
-        }
-        return null;
-    }
+    private final JwtService jwtService;
+    @Autowired
+    private final AuthService authService;
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<User>> loginUser(@RequestHeader("Authorization") String authHeader) {
-        String[] credentials = decodeBasicAuth(authHeader);
-        if (credentials == null) {
+        Optional<User> foundUser = authService.authenticateBasic(authHeader);
+        if (foundUser.isEmpty()) {
             ApiResponse<User> errorResponse = new ApiResponse<>(HttpStatus.UNAUTHORIZED.value(), "error", "Ah Ah Ah! No dijiste la palabra mágica", null);
             return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
         }
-        String username = credentials[0];
-        String password = credentials[1];
-        Optional<User> foundUser = userService.findUserByUsernameAndPassword(username, password);
-
-        if (foundUser.isPresent()) {
-            ApiResponse<User> response = new ApiResponse<>(HttpStatus.OK.value(), "success", "Ha iniciado sesión", foundUser.get());
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }
-
-        ApiResponse<User> errorResponse = new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "error", "Contraseña incorrecta!", null);
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        ApiResponse<User> response = new ApiResponse<>(HttpStatus.OK.value(), "success", "Ha iniciado sesión", foundUser.get());
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping("/register")
     public ResponseEntity<ApiResponse<User>> registerUser(@RequestBody UserDTO userDTO) {
-
         if (userService.findByUsername(userDTO.getUsername()).isPresent()) {
             ApiResponse<User> errorResponse = new ApiResponse<>(HttpStatus.CONFLICT.value(), "error", "El nombre de usuario ya está registrado", null);
             return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
@@ -90,15 +69,15 @@ public class UserController {
         ApiResponse<User> response = new ApiResponse<>(HttpStatus.CREATED.value(), "success", "Usuario registrado exitosamente", null);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
     @PostMapping("/refresh")
     public ResponseEntity<ApiResponse<Map<String, String>>> refreshAccessToken(@RequestBody Map<String, String> request) {
         String refreshToken = request.get("refreshToken");
-        if (jwtService.validateToken(refreshToken)) {
+        if (authService.validateRefreshToken(refreshToken)) {
             String username = jwtService.getUsernameFromToken(refreshToken);
             User user = userService.findByUsername(username).orElseThrow();
             String newAccessToken = jwtService.generateAccessToken(user);
-            Map<String, String> tokens = new HashMap<>();
-            tokens.put("accessToken", newAccessToken);
+            Map<String, String> tokens = Map.of("accessToken", newAccessToken);
             ApiResponse<Map<String, String>> response = new ApiResponse<>(HttpStatus.OK.value(), "success", "Tokens actualizados exitosamente", tokens);
             return new ResponseEntity<>(response, HttpStatus.OK);
         }
@@ -108,51 +87,23 @@ public class UserController {
 
     @GetMapping("/login")
     public ResponseEntity<ApiResponse<User>> loginWithToken(@RequestHeader("Authorization") String authHeader) {
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            ApiResponse<User> errorResponse = new ApiResponse<>(HttpStatus.UNAUTHORIZED.value(), "error", "Ah Ah Ah! No dijiste la palabra mágica", null);
-            return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
-        }
-        String token = authHeader.substring(7);
-        String username = jwtService.getUsernameFromToken(token);
-        if (username == null || !jwtService.validateToken(token)) {
-            ApiResponse<User> errorResponse = new ApiResponse<>(HttpStatus.UNAUTHORIZED.value(), "error", "Token Invalido", null);
-            return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
-        }
-        Optional<User> foundUser = userService.findUserByUsername(username);
+        Optional<User> foundUser = authService.authenticateBearer(authHeader);
         if (foundUser.isEmpty()) {
-            ApiResponse<User> errorResponse = new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "error", "Token invalido", null);
-            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+            ApiResponse<User> errorResponse = new ApiResponse<>(HttpStatus.UNAUTHORIZED.value(), "error", "Token inválido", null);
+            return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
         }
-
         ApiResponse<User> response = new ApiResponse<>(HttpStatus.OK.value(), "success", "Ha iniciado sesión", foundUser.get());
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping("/recovery")
     public ResponseEntity<ApiResponse<String>> recoverPassword(@RequestHeader("Authorization") String authHeader) {
-
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            ApiResponse<String> errorResponse = new ApiResponse<>(HttpStatus.UNAUTHORIZED.value(), "error", "Ah Ah Ah! No dijiste la palabra mágica", "");
-            return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
-        }
-
-        String token = authHeader.substring(7);
-        String username = jwtService.getUsernameFromToken(token);
-
-        if (username == null || !jwtService.validateToken(token)) {
+        Optional<User> foundUser = authService.authenticateBearer(authHeader);
+        if (foundUser.isEmpty()) {
             ApiResponse<String> errorResponse = new ApiResponse<>(HttpStatus.UNAUTHORIZED.value(), "error", "Token Invalido", "");
             return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
         }
-
-        Optional<User> foundUser = userService.findUserByUsername(username);
-        if (foundUser.isEmpty()) {
-            ApiResponse<String> errorResponse = new ApiResponse<>(HttpStatus.NOT_FOUND.value(), "error", "No se encontró al usuario", "");
-            return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-        }
-
         ApiResponse<String> response = new ApiResponse<>(HttpStatus.OK.value(), "success", "Contraseña recuperada", foundUser.get().getPassword());
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
-
-
 }
